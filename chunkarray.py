51f5c6523c9eb0c4adbdfe9c00bd11c500986237
Joe Pringle <explodingp@gmail.com>
2014-02-25 21:33:28 -0500

    Store chat as 'chunks'. Define ChunkArray for analytics.

---
+++ chunkarray.py
@@ -0,0 +1,67 @@
+class Chunk:
+    """
+    Represents data collected from a series of messages in one UNIT_TIME. 
+    self.flagged: number of messages flagged as CAPS or swears. 
+    self.messages:   total NUMBER of messages recieved. NOT individual message contents.
+    """
+
+    def __init__(self, caps, swears, messages):
+        self.flagged = caps + swears
+        self.messages = messages
+
+    def __str__(self):
+        return "FLAGGED: {} OUT OF: {} MESSAGES\n".format(
+                self.flagged, self.messages)
+
+
+class ChunkArray:
+    """
+    Array of chunks. Calling add fills the array to 
+    a specific size, then replaces the oldest element in the array
+
+    self.alloc: number of chunks to allow in array. Not necessarily how
+                many are currently contained.
+
+    self.size: index of oldest element in self.data.
+    self.data: list of chunks
+    """
+
+    def __init__(self, alloc = 50):
+        self.alloc = alloc
+        self.size = 0
+        self.data = []
+
+    def full(self):
+        """
+        Returns True if the number of elements contained (self.size) is equal 
+        to the number of spots allocated. Returns False otherwise.
+        """
+        return (len(self.data) == self.alloc)
+
+    def push(self, i):
+        """ add an element to the array """
+
+        if ( not self.full() ):
+            self.data.append(i)
+
+        else:
+            self.data[ self.size ] = i
+            self.size += 1
+
+            #after stepping through all of self.data, the oldest element is
+            #at position 0
+            if (self.size == self.alloc):
+                self.size = 0
+
+    def flagged(self):
+        """ returns percentage of flagged messages """
+        try:
+            return sum( [c.flagged for c in self.data] ) / sum(
+                    [c.messages for c in self.data])
+
+        except:
+            return 0
+
+
+
+
--- listen.py
+++ listen.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 from pprint import pprint
 from random import randint as ri
+from chunkarray import Chunk, ChunkArray
 
 #=============================================================================
 
@@ -11,17 +12,16 @@ FILES_WRITTEN_TODAY = 0
 LINES_WRITTEN_TO_FILE = 0
 DATE = str(datetime.now()).split()[0]
 
-SYSTEM_TIME = time.time()
-UNIT_TIME = 5  # seconds
+UNIT_TIME = 3  # seconds
 
+# THIS NEEDS TO BE FIXED
+SYSTEM_TIME = time.time()
 SWEARS_PER_UNIT_TIME = 0
 CAPS_PER_UNIT_TIME = 0
 TOTAL_PER_UNIT_TIME = 0
-
 MAX_LINES_PER_FILE = 1000 
 
 FUTURE = time.time() + 2.5*60 + ri(0,60)
-
 
 #=============================================================================
 
@@ -33,7 +33,7 @@ f = open(fname, "a")
 shitlist = set([word.strip().lower() for word in open('swearwords.txt')])
 ignorelist = ['a', 'b', 'start', 'up', 'down', 'left', 'right']
 
-
+recent_chat = ChunkArray()
 
 while (True):
 
@@ -46,7 +46,6 @@ while (True):
 
     #maybe this is where the bottleneck is? Test later
     readbuffer = s.recv(1024).decode("UTF-8", errors="ignore")
-
 
     for msg in readbuffer.split('\n'):
         m = driver.Message(msg)
@@ -66,17 +65,15 @@ while (True):
                 new_fname = driver.getFileName(FILES_WRITTEN_TODAY, DATE)
                 f = open( new_fname, "a")
 
-
             if (time.time() >= SYSTEM_TIME + UNIT_TIME):
-                print( "Swears per unit time: {}".format(SWEARS_PER_UNIT_TIME) )
-                print( "CAPS   per unit time: {}".format(CAPS_PER_UNIT_TIME) )
-                print( "TOTAL  per unit time: {}".format(TOTAL_PER_UNIT_TIME) )
-                print("--")
-                f.write( "Swears per unit time: {}\n".format(SWEARS_PER_UNIT_TIME) )
-                f.write( "CAPS   per unit time: {}\n".format(CAPS_PER_UNIT_TIME) )
-                f.write( "TOTAL  per unit time: {}\n".format(TOTAL_PER_UNIT_TIME) )
-                f.write("--\n")
-                LINES_WRITTEN_TO_FILE += 4
+                C = Chunk(CAPS_PER_UNIT_TIME, SWEARS_PER_UNIT_TIME, 
+                        TOTAL_PER_UNIT_TIME)
+
+                print("%.2f percent" %(100*recent_chat.flagged()))
+                recent_chat.push(C)
+
+                f.write( str(C) )
+                LINES_WRITTEN_TO_FILE +=  1
 
                 SYSTEM_TIME = time.time()
                 SWEARS_PER_UNIT_TIME = 0
@@ -87,18 +84,9 @@ while (True):
                 continue
 
             if ( m.isSwear(shitlist) ):
-                f.write(str(m)+'\n')
-                LINES_WRITTEN_TO_FILE += 1
-                print(m)
                 SWEARS_PER_UNIT_TIME += 1
 
-            #elif maybe
-            if ( m.isAllCaps() ):
-                f.write(str(m)+'\n')
-                LINES_WRITTEN_TO_FILE += 1
-                print(m)
+            elif ( m.isAllCaps() ):
                 CAPS_PER_UNIT_TIME += 1
-
-
 
             #print("{} {} {}\n".format(m.time, m.sender, m.contents))
